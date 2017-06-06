8651c902d1d7dc7a3e9f8821a1389e493800f34f
perfetti <perfettiphotography@gmail.com>
2017-06-06 16:35:11 -0400

    updating redis_url

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module GlamourShots
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.cache_store = :redis_store, "redis://localhost:6379/0/cache", { expires_in: 7.days }
+    config.cache_store = :redis_store, ENV['REDIS_URL'], { expires_in: 7.days }
   end
 end
